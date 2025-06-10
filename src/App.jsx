63bb502666a7a689e3700e7534ec4d2457537484
FrancescoMorettiBrs <morettifrancesco.lav@gmail.com>
2025-06-10 20:57:26 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+function App() {
+  const apiUrlAttori = "https://lanciweb.github.io/demo/api/actors/](https://lanciweb.github.io/demo/api/actors/"
+  const apiUrlAttrici = "https://lanciweb.github.io/demo/api/actresses/](https://lanciweb.github.io/demo/api/actresses/"
+  useEffect(axios.get(apiUrlAttori)then)
+  
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
